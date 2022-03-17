bea17b49304ebb9730d055279e76b177eb93cda1
ngelelilitha18@gmail.com <ngelelilitha18@gmail.com>
2022-03-17 13:10:18 +0200

    add nodemailer

--- routes/contactRouter.js
+++ routes/contactRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
-const app = express.Router();
+const router = express.Router();
 
 router.get("/", (req, res) => {
   res.send({ msg: "getting contacts" });
