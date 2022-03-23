9ceedb5551a08631c85e1755c0d5af7960196e70
ngelelilitha18@gmail.com <ngelelilitha18@gmail.com>
2022-03-23 15:09:06 +0200

    add new deleted items

--- routes/blogs.js
+++ routes/blogs.js
@@ -48,7 +48,7 @@ router.patch("/:id", getBlogs, async (req, res) => {
     res.blogs.img = req.body.img;
   }
   try {
-    const updatedBlog = await res.blogs.update();
+    const updatedBlog = await res.blogs.save();
     res.json(updatedBlog);
   } catch (err) {
     res.status(400).json({ message: err.message });
