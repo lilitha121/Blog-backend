d5be2f73f21a62ac07f9630540a80c58d34405cf
ngelelilitha18@gmail.com <ngelelilitha18@gmail.com>
2022-03-23 15:41:54 +0200

    add new files

--- routes/blogs.js
+++ routes/blogs.js
@@ -48,7 +48,7 @@ router.patch("/:id", getBlogs, async (req, res) => {
     res.blogs.img = req.body.img;
   }
   try {
-    const updatedBlog = await res.blogs.save();
+    const updatedBlog = await res.blogs.findByIdAndUpdate();
     res.json(updatedBlog);
   } catch (err) {
     res.status(400).json({ message: err.message });
